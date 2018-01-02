8979b511ffa5e64befc678985e8f8f36c5fa245b
changlu <luzhang01@lianjia.com>
2018-01-02 18:16:08 +0800

    ZoneId

--- src/main/java/com/changlu/ZoneIdDemo.java
+++ src/main/java/com/changlu/ZoneIdDemo.java
@@ -13,7 +13,7 @@ public class ZoneIdDemo {
         ZoneId zoneId = ZoneId.systemDefault();
         System.out.println("ZoneId.systemDefault() = " + zoneId);
         System.out.println("TimeZone.getDefault().toZoneId() = " + TimeZone.getDefault().toZoneId());
-        zoneId = ZoneId.of("Europe/Rome");
+        zoneId = ZoneId.of("Asia/Shanghai");
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("localDateTime = " + localDateTime);
         System.out.println("localDateTime.atZone(zoneId) = " + localDateTime.atZone(zoneId));
